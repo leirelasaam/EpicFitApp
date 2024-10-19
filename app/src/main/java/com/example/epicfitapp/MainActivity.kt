f00b0b704a8aff52f00c360c817c81a60aaea09d
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-10-19 13:55:02 +0200

    Añadir conexión y crear ejemplo
    Establecer la configuración de archivos gradle para utilizar las dependencias necesarias.
    Probar la conexión con ejemplo.

--- app/src/main/java/com/example/epicfitapp/MainActivity.kt
+++ app/src/main/java/com/example/epicfitapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.epicfitapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        getAllUsers { result ->
+            if (result != null) {
+                Log.d("MainActivity", "Usuarios obtenidos.")
+            } else {
+                Log.d("MainActivity", "No se pudieron obtener los usuarios.")
+            }
+        }
     }
+
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val collectionName = "Usuarios"
+
+    private fun getAllUsers(onResult: (QuerySnapshot?) -> Unit) {
+        db.collection(collectionName)
+            .get()
+            .addOnSuccessListener { result ->
+                onResult(result)
+
+                // Verificar si hay documentos en la consulta
+                if (result != null && !result.isEmpty) {
+                    // Iterar sobre los documentos obtenidos
+                    for (document in result.documents) {
+                        val userId = document.id
+                        val userData = document.data
+
+                        Log.d("FirestoreService", "User ID: $userId, Data: $userData")
+                    }
+                } else {
+                    Log.d("FirestoreService", "No users found.")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.w("FirestoreService", "Error getting documents.", e)
+                onResult(null)
+            }
+    }
+
+
 }
