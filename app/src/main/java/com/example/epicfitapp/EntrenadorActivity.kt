2ad1852465e43a49d7d5a624bc5e747da9f8c7aa
Leire <leire.lasaam@elorrieta-errekamari.com>
2024-10-25 19:26:41 +0200

    Evitar rotación y gestionar ids de layouts correctamente

--- app/src/main/java/com/example/epicfitapp/EntrenadorActivity.kt
+++ app/src/main/java/com/example/epicfitapp/EntrenadorActivity.kt
@@ -11,7 +11,7 @@ class EntrenadorActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_entrenador)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.entrenador)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
