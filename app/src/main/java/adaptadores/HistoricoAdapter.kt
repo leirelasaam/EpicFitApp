7b1663ad488e39c400fc5d6d01b0e61cc5509c8b
Leire <leire.lasaam@elorrieta-errekamari.com>
2024-11-04 15:31:20 +0100

    Arreglar botón play

--- app/src/main/java/adaptadores/HistoricoAdapter.kt
+++ app/src/main/java/adaptadores/HistoricoAdapter.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -74,6 +75,7 @@ class HistoricoAdapter(private val context: Context?, private var historicos: Li
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl))
                 context?.startActivity(intent)
             }
+            holder.btnPlay.isVisible = true
         } else {
             holder.btnPlay.isVisible = false
         }
